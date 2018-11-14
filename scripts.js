e1df318e7a448dfe3b3248f6f11e0b486fad1762
TomNPython <42147472+TomNPython@users.noreply.github.com>
2018-11-14 13:28:18 +0700

    added comments

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 const displayField = document.getElementById('display-field')
 const operators = ['+', '-', '*', '/', '.']
 
+// update the display field initially only if the first button clicked is not an operator
 function updateDisplayField(button) { 
   if (displayField.innerHTML.length == 0 && operators.indexOf(button) == -1) {
   displayField.innerHTML += button
@@ -8,6 +9,7 @@ function updateDisplayField(button) {
   displayField.innerHTML += button
 }}
 
+//display the result of a calculation if it is a number
 function getResult() {
   if (isNaN(eval(displayField.innerHTML))) {
   displayField.innerHTML = 'Error'
@@ -15,10 +17,12 @@ function getResult() {
   displayField.innerHTML = eval(displayField.innerHTML)
 }}
 
+//remove what is shown in the display field
 function clearDisplay() {
   displayField.innerHTML = ''
 }
 
+//remove one item from the display field
 function delOne() {
   displayField.innerHTML = displayField.innerHTML.slice(0, -1)
 }
